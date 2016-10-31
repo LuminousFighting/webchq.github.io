dbbb1a339685d1bdd2c588765e24788e56cb43c8
玮 <xitiantianxia@gmail.com>
2016-10-31 18:51:32 +0800

    整体更新
    较大的一次整体更新

--- javascripts/test.js
+++
@@ -1,7 +0,0 @@
-var oDiv = document.getElementsByClassName("oDiv")[0];
-oDiv.onmouseenter=function(){
-	oDiv.style.background="red";
-}
-oDiv.onmouseleave=function(){
-	oDiv.style.background="white"
-}
